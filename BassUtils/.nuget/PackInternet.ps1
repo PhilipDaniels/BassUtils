4e226bb6b2624af6cbffe04625c21042b19728a3
Philip Daniels <Philip.Daniels1971@gmail.com>
2015-04-26 14:48:33 +0100

    Update to latest version of PowerPack, build both 40 and 45 versions.

--- BassUtils/.nuget/PackInternet.ps1
+++
@@ -1,8 +0,0 @@
-Import-Module "$PSScriptRoot\PowerPack" -Force
-
-# Edit this file to suit your projects. See the README for more info.
-$projects = @("BassUtils")
-$options.NuGetFeed = ""
-$options.DoPush = $true
-PowerPack $projects
-
--- BassUtils/BassUtils/ConfigurationLoader.cs
+++ BassUtils/BassUtils/ConfigurationLoader.cs
@@ -313,3 +313,4 @@ namespace BassUtils
         }
     }
 }
+
