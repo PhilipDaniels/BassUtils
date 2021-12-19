1dd51f836029c2ef59e96c618a4e6279eb3f7665
Philip Daniels <Philip.Daniels1971@gmail.com>
2021-12-19 17:04:22 +0000

    Working on Oracle params.

---
+++ BassUtils.Oracle/OracleParameterCollectionExtensions.AddArrayParameters.cs
@@ -0,0 +1,90 @@
+﻿using System.Data;
+using Dawn;
+using Oracle.ManagedDataAccess.Client;
+
+namespace BassUtils.Oracle;
+
+/// <summary>
+/// Extensions for the <c>OracleParameterCollection</c> class.
+/// </summary>
+public static partial class OracleParameterCollectionExtensions
+{
+    /// <summary>
+    /// Adds an OracleParameter that corresponds to array binding.
+    /// This does not require any server-side definitions, it works by sending a single
+    /// SQL statement and multiple sets of data in one network call.
+    /// See https://docs.oracle.com/en/database/oracle/oracle-database/18/odpnt/featOraCommand.html#GUID-FACB870D-6F8B-46EA-95EA-65C6C6536B9E
+    /// Section 3.8.3.5 Array Binding.
+    /// <b>WARNING: Caller must set the ArrayBindCount property on the containing command to the length of the <paramref name="values"/> array,
+    /// which means all arrays passed to the statement must be the same length.</b>
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the values collection.</typeparam>
+    /// <param name="parameterCollection">The collection to add the parameter to.</param>
+    /// <param name="parameterName">The name of the parameter.</param>
+    /// <param name="oracleDbType">The corresponding Oracle type of the values.</param>
+    /// <param name="parameterDirection">The direction of the parameter.</param>
+    /// <param name="values">The values in the array.</param>
+    /// <returns>The parameter that was added.</returns>
+    public static OracleParameter AddArrayParameter<T>
+        (
+        this OracleParameterCollection parameterCollection,
+        string parameterName,
+        OracleDbType oracleDbType,
+        ParameterDirection parameterDirection,
+        IEnumerable<T> values
+        )
+    {
+        Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
+        Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
+        Guard.Argument(values, nameof(values)).NotNull();
+
+        var prm = new OracleParameter();
+        prm.ParameterName = parameterName;
+        prm.Direction = parameterDirection;
+        prm.OracleDbType = oracleDbType;
+        prm.Value = values;
+        parameterCollection.Add(prm);
+        return prm;
+    }
+
+    /// <summary>
+    /// Adds an OracleParameter that corresponds to array binding.
+    /// This does not require any server-side definitions, it works by sending a single
+    /// SQL statement and multiple sets of data in one network call.
+    /// See https://docs.oracle.com/en/database/oracle/oracle-database/18/odpnt/featOraCommand.html#GUID-FACB870D-6F8B-46EA-95EA-65C6C6536B9E
+    /// Section 3.8.3.5 Array Binding.
+    /// <b>WARNING: Caller must set the ArrayBindCount property on the containing command to the length of the <paramref name="values"/> array,
+    /// which means all arrays passed to the statement must be the same length.</b>
+    /// </summary>
+    /// <remarks>
+    /// This string overload sets the OracleDbType to Varchar2 and automatically calculates
+    /// the length of each string in the array.
+    /// </remarks>
+    /// <param name="parameterCollection">The collection to add the parameter to.</param>
+    /// <param name="parameterName">The name of the parameter.</param>
+    /// <param name="parameterDirection">The direction of the parameter.</param>
+    /// <param name="values">The values in the array.</param>
+    /// <returns>The parameter that was added.</returns>
+    public static OracleParameter AddArrayParameter
+        (
+        this OracleParameterCollection parameterCollection,
+        string parameterName,
+        ParameterDirection parameterDirection,
+        IEnumerable<string> values
+        )
+    {
+        Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
+        Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
+        Guard.Argument(values, nameof(values)).NotNull();
+
+        var prm = new OracleParameter();
+        prm.ParameterName = parameterName;
+        prm.Direction = parameterDirection;
+        prm.OracleDbType = OracleDbType.Varchar2;
+        prm.Value = values;
+        // ArrayBindSize is the length of each string.
+        prm.ArrayBindSize = GetStringLengths(values);
+        parameterCollection.Add(prm);
+        return prm;
+    }
+}
--- BassUtils.Oracle/OracleParameterCollectionExtensions.cs
+++ BassUtils.Oracle/OracleParameterCollectionExtensions.cs
@@ -5,6 +5,13 @@ using Oracle.ManagedDataAccess.Types;
 
 namespace BassUtils.Oracle;
 
+/*
+ * TODO
+ * - AddUdtObjectParameter (how do we pass a null object?)
+ * - AddUdtArrayParameter (passing a null array makes no sense, but how do we pass an array that contains a null object?)
+ */
+
+
 /// <summary>
 /// Extensions for the <c>OracleParameterCollection</c> class.
 /// </summary>
@@ -19,6 +26,14 @@ public static partial class OracleParameterCollectionExtensions
     }
 
     /// <summary>
+    /// Returns the length of every string in the collection.
+    /// </summary>
+    private static int[] GetStringLengths(IEnumerable<string> values)
+    {
+        return values.Select(d => d.Length).ToArray();
+    }
+
+    /// <summary>
     /// Adds an OracleParameter that corresponds to a single Oracle UDT,
     /// i.e. one defined at schema-level using the "CREATE TYPE objXyz AS OBJECT" syntax.
     /// </summary>
@@ -27,7 +42,7 @@ public static partial class OracleParameterCollectionExtensions
     /// <param name="parameterName">The name of the parameter.</param>
     /// <param name="udtTypeName">The name of the UDT type. This will be a two-part name of the
     /// form "SCHEMA.OBJECTTYPENAME".</param>
-    /// <param name="value">The value of the parameter.</param>
+    /// <param name="value">The value of the parameter. Must be a special <c>IOracleCustomType</c> implementation.</param>
     /// <param name="parameterDirection">The direction of the parameter.</param>
     /// <returns>The parameter that was added.</returns>
     public static OracleParameter AddUdtObjectParameter<T>
@@ -63,7 +78,8 @@ public static partial class OracleParameterCollectionExtensions
     /// <param name="parameterName">The name of the parameter.</param>
     /// <param name="udtTypeName">The name of the UDT type. This will be a two-part name of the
     /// form "SCHEMA.TABLETYPENAME".</param>
-    /// <param name="value"></param>
+    /// <param name="value">The value of the parameter. Must be a special <c>IOracleCustomType</c> implementation
+    /// that is a collection type.</param>
     /// <param name="parameterDirection">The direction of the parameter.</param>
     /// <returns>The parameter that was added.</returns>
     public static OracleParameter AddUdtArrayParameter<T>(
@@ -87,259 +103,5 @@ public static partial class OracleParameterCollectionExtensions
         prm.Value = value;
         parameterCollection.Add(prm);
         return prm;
-    }
-
-    /// <summary>
-    /// Adds an OracleParameter that corresponds to an associative array,
-    /// i.e. one defined at PACKAGE level using the "TYPE arrXYZ IS TABLE OF abc INDEX BY ..." syntax.
-    /// Such arrays may be used at package or schema level.
-    /// </summary>
-    /// <typeparam name="T">The type of the elements in the values collection.</typeparam>
-    /// <param name="parameterCollection">The collection to add the parameter to.</param>
-    /// <param name="parameterName">The name of the parameter.</param>
-    /// <param name="parameterDirection">The direction of the parameter.</param>
-    /// <param name="values">The values to set as the parameter value.</param>
-    /// <returns>The parameter that was added.</returns>
-    public static OracleParameter AddAssociativeArrayParameter<T>(
-      this OracleParameterCollection parameterCollection,
-      string parameterName,
-      ParameterDirection parameterDirection,
-      params T[] values
-      )
-    {
-        Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
-        Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
-        Guard.Argument(values, nameof(values)).NotNull();
-
-        var prm = new OracleParameter();
-        prm.ParameterName = parameterName;
-        prm.Direction = parameterDirection;
-        prm.CollectionType = OracleCollectionType.PLSQLAssociativeArray;
-        prm.Value = values;
-        prm.Size = values.Length;
-        parameterCollection.Add(prm);
-        return prm;
-    }
-
-    /// <summary>
-    /// Adds an OracleParameter that corresponds to an associative array,
-    /// i.e. one defined at PACKAGE level using the "TYPE arrXYZ IS TABLE OF abc INDEX BY ..." syntax.
-    /// Such arrays may be used at package or schema level.
-    /// </summary>
-    /// <typeparam name="T">The type of the elements in the values collection.</typeparam>
-    /// <param name="parameterCollection">The collection to add the parameter to.</param>
-    /// <param name="parameterName">The name of the parameter.</param>
-    /// <param name="parameterDirection">The direction of the parameter.</param>
-    /// <param name="values">The values to set as the parameter value.</param>
-    /// <returns>The parameter that was added.</returns>
-    public static OracleParameter AddAssociativeArrayParameter<T>(
-      this OracleParameterCollection parameterCollection,
-      string parameterName,
-      ParameterDirection parameterDirection,
-      IEnumerable<T> values
-      )
-    {
-        Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
-        Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
-        Guard.Argument(values, nameof(values)).NotNull();
-
-        return AddAssociativeArrayParameter(parameterCollection, parameterName, parameterDirection, values.ToArray());
-    }
-
-    /// <summary>
-    /// Adds an OracleParameter that corresponds to an associative array of strings.
-    /// i.e. one defined at PACKAGE level using the "TYPE arrXYZ IS TABLE OF VARCHAR2(n) INDEX BY ..." syntax.
-    /// Such arrays may be used at package or schema level.
-    /// </summary>
-    /// <param name="parameterCollection">The collection to add the parameter to.</param>
-    /// <param name="parameterName">The name of the parameter.</param>
-    /// <param name="parameterDirection">The direction of the parameter.</param>
-    /// <param name="values">The values to set as the parameter value.</param>
-    /// <returns>The parameter that was added.</returns>
-    public static OracleParameter AddAssociativeArrayParameter(
-      this OracleParameterCollection parameterCollection,
-      string parameterName,
-      ParameterDirection parameterDirection,
-      params string[] values
-  )
-    {
-        Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
-        Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
-        Guard.Argument(values, nameof(values)).NotNull();
-
-        var prm = new OracleParameter();
-        prm.ParameterName = parameterName;
-        prm.Direction = parameterDirection;
-        prm.CollectionType = OracleCollectionType.PLSQLAssociativeArray;
-        prm.Value = values;
-        prm.Size = values.Length;
-        // ArrayBindSize is the length of each string.
-        prm.ArrayBindSize = GetStringLengths(values);
-        parameterCollection.Add(prm);
-        return prm;
-    }
-
-    /// <summary>
-    /// Adds an OracleParameter that corresponds to an associative array of strings.
-    /// i.e. one defined at PACKAGE level using the "TYPE arrXYZ IS TABLE OF VARCHAR2(n) INDEX BY ..." syntax.
-    /// Such arrays may be used at package or schema level.
-    /// </summary>
-    /// <param name="parameterCollection">The collection to add the parameter to.</param>
-    /// <param name="parameterName">The name of the parameter.</param>
-    /// <param name="parameterDirection">The direction of the parameter.</param>
-    /// <param name="values">The values to set as the parameter value.</param>
-    /// <returns>The parameter that was added.</returns>
-    public static OracleParameter AddAssociativeArrayParameter(
-      this OracleParameterCollection parameterCollection,
-      string parameterName,
-      ParameterDirection parameterDirection,
-      IEnumerable<string> values
-      )
-    {
-        Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
-        Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
-        Guard.Argument(values, nameof(values)).NotNull();
-
-        return AddAssociativeArrayParameter(parameterCollection, parameterName, parameterDirection, values.ToArray());
-    }
-
-    /// <summary>
-    /// Adds an OracleParameter that corresponds to array binding.
-    /// This does not require any server-side definitions, it works by sending a single
-    /// SQL statement and multiple sets of data in one network call.
-    /// See https://docs.oracle.com/en/database/oracle/oracle-database/18/odpnt/featOraCommand.html#GUID-FACB870D-6F8B-46EA-95EA-65C6C6536B9E
-    /// Section 3.8.3.5 Array Binding.
-    /// <b>WARNING: Caller must set the ArrayBindCount property on the containing command to the length of the <paramref name="values"/> array,
-    /// which means all arrays passed to the statement must be the same length.</b>
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="parameterCollection">The collection to add the parameter to.</param>
-    /// <param name="parameterName">The name of the parameter.</param>
-    /// <param name="oracleDbType">The corresponding Oracle type of the values.</param>
-    /// <param name="parameterDirection">The direction of the parameter.</param>
-    /// <param name="values">The values in the array.</param>
-    /// <returns>The parameter that was added.</returns>
-    public static OracleParameter AddArrayParameter<T>(
-      this OracleParameterCollection parameterCollection,
-      string parameterName,
-      OracleDbType oracleDbType,
-      ParameterDirection parameterDirection,
-      params T[] values
-      )
-    {
-        Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
-        Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
-        Guard.Argument(values, nameof(values)).NotNull();
-
-        var prm = new OracleParameter();
-        prm.ParameterName = parameterName;
-        prm.Direction = parameterDirection;
-        prm.OracleDbType = oracleDbType;
-        prm.Value = values;
-        parameterCollection.Add(prm);
-        return prm;
-    }
-
-    /// <summary>
-    /// Adds an OracleParameter that corresponds to array binding.
-    /// This does not require any server-side definitions, it works by sending a single
-    /// SQL statement and multiple sets of data in one network call.
-    /// See https://docs.oracle.com/en/database/oracle/oracle-database/18/odpnt/featOraCommand.html#GUID-FACB870D-6F8B-46EA-95EA-65C6C6536B9E
-    /// Section 3.8.3.5 Array Binding.
-    /// <b>WARNING: Caller must set the ArrayBindCount property on the containing command to the length of the <paramref name="values"/> array,
-    /// which means all arrays passed to the statement must be the same length.</b>
-    /// </summary>
-    /// <typeparam name="T">The type of elements in the values.</typeparam>
-    /// <param name="parameterCollection">The collection to add the parameter to.</param>
-    /// <param name="parameterName">The name of the parameter.</param>
-    /// <param name="oracleDbType">The corresponding Oracle type of the values.</param>
-    /// <param name="parameterDirection">The direction of the parameter.</param>
-    /// <param name="values">The values in the array.</param>
-    /// <returns>The parameter that was added.</returns>
-    public static OracleParameter AddArrayParameter<T>(
-      this OracleParameterCollection parameterCollection,
-      string parameterName,
-      OracleDbType oracleDbType,
-      ParameterDirection parameterDirection,
-      IEnumerable<T> values
-      )
-    {
-        Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
-        Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
-        Guard.Argument(values, nameof(values)).NotNull();
-
-        return AddArrayParameter(parameterCollection, parameterName, oracleDbType, parameterDirection, values.ToArray());
-    }
-
-    /// <summary>
-    /// Adds an OracleParameter that corresponds to array binding.
-    /// This does not require any server-side definitions, it works by sending a single
-    /// SQL statement and multiple sets of data in one network call.
-    /// See https://docs.oracle.com/en/database/oracle/oracle-database/18/odpnt/featOraCommand.html#GUID-FACB870D-6F8B-46EA-95EA-65C6C6536B9E
-    /// Section 3.8.3.5 Array Binding.
-    /// <b>WARNING: Caller must set the ArrayBindCount property on the containing command to the length of the <paramref name="values"/> array,
-    /// which means all arrays passed to the statement must be the same length.</b>
-    /// </summary>
-    /// <remarks>
-    /// This string overload sets the OracleDbType to Varchar2 and automatically calculates
-    /// the length of each string in the array.
-    /// </remarks>
-    /// <param name="parameterCollection">The collection to add the parameter to.</param>
-    /// <param name="parameterName">The name of the parameter.</param>
-    /// <param name="parameterDirection">The direction of the parameter.</param>
-    /// <param name="values">The values in the array.</param>
-    /// <returns>The parameter that was added.</returns>
-    public static OracleParameter AddArrayParameter(
-      this OracleParameterCollection parameterCollection,
-      string parameterName,
-      ParameterDirection parameterDirection,
-      params string[] values
-      )
-    {
-        Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
-        Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
-        Guard.Argument(values, nameof(values)).NotNull();
-
-        var prm = new OracleParameter();
-        prm.ParameterName = parameterName;
-        prm.Direction = parameterDirection;
-        prm.OracleDbType = OracleDbType.Varchar2;
-        prm.Value = values;
-        // ArrayBindSize is the length of each string.
-        prm.ArrayBindSize = GetStringLengths(values);
-        parameterCollection.Add(prm);
-        return prm;
-    }
-
-    /// <summary>
-    /// Adds an OracleParameter that corresponds to array binding.
-    /// This does not require any server-side definitions, it works by sending a single
-    /// SQL statement and multiple sets of data in one network call.
-    /// See https://docs.oracle.com/en/database/oracle/oracle-database/18/odpnt/featOraCommand.html#GUID-FACB870D-6F8B-46EA-95EA-65C6C6536B9E
-    /// Section 3.8.3.5 Array Binding.
-    /// <b>WARNING: Caller must set the ArrayBindCount property on the containing command to the length of the <paramref name="values"/> array,
-    /// which means all arrays passed to the statement must be the same length.</b>
-    /// </summary>
-    /// <remarks>
-    /// This string overload sets the OracleDbType to Varchar2 and automatically calculates
-    /// the length of each string in the array.
-    /// </remarks>
-    /// <param name="parameterCollection">The collection to add the parameter to.</param>
-    /// <param name="parameterName">The name of the parameter.</param>
-    /// <param name="parameterDirection">The direction of the parameter.</param>
-    /// <param name="values">The values in the array.</param>
-    /// <returns>The parameter that was added.</returns>
-    public static OracleParameter AddArrayParameter(
-      this OracleParameterCollection parameterCollection,
-      string parameterName,
-      ParameterDirection parameterDirection,
-      IEnumerable<string> values
-      )
-    {
-        Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
-        Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
-        Guard.Argument(values, nameof(values)).NotNull();
-
-        return AddArrayParameter(parameterCollection, parameterName, parameterDirection, values.ToArray());
     }
 }
