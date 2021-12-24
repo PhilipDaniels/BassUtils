fa8d77b4e1e82de48056ebf290f6b26f450708dd
Philip Daniels <Philip.Daniels1971@gmail.com>
2021-12-24 13:20:49 +0000

    Add TVP for MS SQL.

--- BassUtils/MsSql/SqlParameterCollectionExtensions.cs
+++ BassUtils/MsSql/SqlParameterCollectionExtensions.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dawn;
 using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient.Server;
 
 namespace BassUtils.MsSql
 {
@@ -12,23 +17,60 @@ namespace BassUtils.MsSql
         /// Adds a value to the end of the <c>SqlParameterCollection</c>.
         /// Uses <c>DBNull.Value</c> if the <paramref name="value"/> is null.
         /// </summary>
-        /// <param name="collection">The parameter collection to add to.</param>
+        /// <param name="parameterCollection">The parameter collection to add to.</param>
         /// <param name="parameterName">The name of the parameter.</param>
         /// <param name="value">The value to be added. DBNull.Value will be used for null values.</param>
-        /// <returns>A Microsoft.Data.SqlClient.SqlParameter object.</returns>
+        /// <returns>The new SqlParameter object.</returns>
         public static SqlParameter AddWithNullableValue(
-            this SqlParameterCollection collection,
+            this SqlParameterCollection parameterCollection,
             string parameterName,
             object value)
         {
+            Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
+            Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
+
             if (value == null)
             {
-                return collection.AddWithValue(parameterName, DBNull.Value);
+                return parameterCollection.AddWithValue(parameterName, DBNull.Value);
             }
             else
             {
-                return collection.AddWithValue(parameterName, value);
+                return parameterCollection.AddWithValue(parameterName, value);
             }
+        }
+
+        /// <summary>
+        /// Adds a table-valued parameter to the end of the <c>SqlParameterCollection</c>.
+        /// </summary>
+        /// <param name="parameterCollection">The parameter collection to add to.</param>
+        /// <param name="parameterName">The name of the parameter.</param>
+        /// <param name="typeName">The name of the custom table type in SQL Server.</param>
+        /// <param name="records">The collection of records to add as the parameter value.</param>
+        /// <returns>The new SqlParameter object.</returns>
+        public static SqlParameter AddTableValuedParameter(
+            this SqlParameterCollection parameterCollection,
+            string parameterName,
+            string typeName,
+            IEnumerable<SqlDataRecord> records
+            )
+        {
+            Guard.Argument(parameterCollection, nameof(parameterCollection)).NotNull();
+            Guard.Argument(parameterName, nameof(parameterName)).NotNull().NotWhiteSpace();
+            Guard.Argument(typeName, nameof(typeName)).NotNull().NotWhiteSpace();
+
+            var prm = parameterCollection.Add(parameterName, SqlDbType.Structured);
+            prm.TypeName = typeName;
+
+            if (records == null)
+            {
+                prm.Value = DBNull.Value;
+            }
+            else
+            {
+                prm.Value = records.ToArray();
+            }
+
+            return prm;
         }
     }
 }
