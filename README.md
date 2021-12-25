aad82948fb922656aa2d9fdcdaee5c92a99bce88
Philip Daniels <Philip.Daniels1971@gmail.com>
2021-12-25 16:14:43 +0000

    Update clear_package_cache.ps1 to work on Linux.

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ Available on [NuGet](https://www.nuget.org/packages/BassUtils.NetCore)
 
 
 See individual READMEs for more information.
+
+
+# How to publish and version - aide-mémoire
+
+If version 4.1 is published on nuget.org, then to start the next version:
+
+- Make a branch v4.2
+- Update version.json to read:    "version": "4.2-alpha"
+- Make commits on that branch and build using Nerdbank.GitVersioning - they
+  will come out as 4.2.x-alpha-GUID.nupkg.
+- Use clear_package_cache.ps1 to facilitate local development.
+- When done, change version.json to "4.2"
+- Merge down to master branch and build - packages should be at version 4.2.
+- Upload packages to NuGet.
--- clear_package_cache.ps1
+++ clear_package_cache.ps1
@@ -2,19 +2,48 @@
 # Removes a package from the NuGet package cache.
 # This allows it to be re-installed in a project (after a clean)
 # without having to bump the version number.
+#
+#
+# List the location of the global package cache
+#     dotnet nuget locals global-packages --list
+#
+# List the location of all NuGet caches
+#     dotnet nuget locals all --list
+#
+# Clear the global package cache (clears all packages)
+#     dotnet nuget locals global-packages --clear
+#
+# Clear all NuGet caches
+#     dotnet nuget locals all --clear
 
-# Clear the entire package cache: nuget locals <all | http-cache | packages-cache | global-packages> -clear
 
 if (!$args)
 {
-	Write-Host "Specify name(s) of package as command line parameter(s)"
-	Write-Host "  e.g. purge_nuget_package.ps1 BassUtils BassUtils.NetCore log4net"
+	Write-Host "Specify name(s) of package as command line parameter(s):"
+	Write-Host ""
+	Write-Host "  ./clear_package_cache.ps1 BassUtils BassUtils.NetCore log4net"
+	Write-Host ""
+    Write-Host "Alternatively, clear **all** packages from the global cache with:"
+	Write-Host ""
+    Write-Host "  ./clear_package_cache.ps1 ALL"
+	Write-Host ""
 	exit
 }
 
+# Outputs:    global-packages: C:\Users\phil\.nuget\packages\
+$cache_dir = (dotnet nuget locals global-packages --list) | Out-String
+$cache_dir = $cache_dir.Split(':', 2)[1].Trim();
+
+if ($args.Length -eq 1 -and $args[0] -eq "ALL")
+{
+	dotnet nuget locals global-packages --clear
+    exit
+}
+
+
 foreach ($package in $args)
 {
-	$pkgdir = "C:\Users\pdaniels\.nuget\packages\$package"
+	$pkgdir = "$cache_dir$package"
 	
 	if (Test-Path $pkgdir)
 	{
@@ -26,4 +55,3 @@ foreach ($package in $args)
 		Write-Host "Package directory $pkgdir does not exist, nothing to do"
 	}
 }
-
